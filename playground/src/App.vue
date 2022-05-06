37061227a6eb32866da059854865ce8057429946
zhuh <zhuh@wingoht.com>
2022-05-06 12:49:09 +0800

    菜单样式

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -8,7 +8,7 @@ export default {
 </script>
 
 <template>
-  <div class="w-screen h-screen flex flex-col min-w-6xl">
+  <div class="w-screen h-screen relative flex flex-col min-w-6xl">
     <PlaygroundMenu />
     <div class="flex-1 bg-[#fdfdfd] overflow-auto">
       <router-view></router-view>
