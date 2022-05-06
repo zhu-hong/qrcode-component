1b0ad0d41593169f1780523262bd07f702cb9bcc
zhuh <zhuh@wingoht.com>
2022-05-06 09:15:01 +0800

    二维码数据修改

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -3,69 +3,45 @@ export default {
   data() {
     return {
       qrCodeInfo: {
-        "id": "45LEPiornVp",
-        "name": "工位c",
-        "qrAppDatas": [
-          {
-            "appId": "202010151636001",
-            "appName": "企业模型",
-            "isExpire": false,
-            "categoryItems": [
-              {
-                "isSystem": true,
-                "category": "工作单元",
-                "fields": [
-                  {
-                    "key": "id",
-                    "key216": "id",
-                    "desc": "工作单元ID",
-                    "value": "3pu2XJs4LEj"
-                  }
-                ]
-              }
-            ]
-          }
-        ],
-        "events": [],
-        "state": 0,
-        "appName": "二维码生成器",
-        "creator": "cy",
+        "id": "4XvmuiUeC2b",
         "model": {
           "config": {
-            "id": "color5",
-            "type": 1,
-            "name": "彩色标签5",
-            "svg": "<svg width=\"100%\" height=\"100%\" viewBox=\"0 0 600 400\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">\n  <g stroke=\"none\" stroke-width=\"1\" fill=\"none\" fill-rule=\"evenodd\">\n    <rect fill=\"#FFFFFF\" x=\"0\" y=\"0\" width=\"600\" height=\"400\"></rect>\n    <rect fill=\"#254569\" data-type=\"bg\" x=\"0\" y=\"0\" width=\"600\" height=\"400\"></rect>\n    <rect fill=\"#FFFFFF\" x=\"24\" y=\"24\" width=\"229\" height=\"352\" rx=\"12\"></rect>\n\n    <g transform=\"translate(75,210)\">\n      <image x=\"0\" y=\"0\" width=\"128\" height=\"128\" data-type=\"qr\" xlink:href=\"\"></image>\n    </g>\n    <g transform=\"translate(114, 75)\">\n      <rect x=\"0\" y=\"0\" width=\"48\" height=\"48\" fill=\"#FFF\"></rect>\n      <image x=\"0\" y=\"0\" width=\"48\" height=\"48\" data-type=\"logo\" xlink:href=\"\"></image>\n    </g>\n\n    <text font-family=\"微软雅黑\" font-size=\"38\" font-weight=\"500\" fill=\"#FFF\">\n      <tspan x=\"285\" y=\"84\" data-type=\"title\" data-max=\"44\" data-len=\"7\"></tspan>\n      <tspan x=\"285\" y=\"84\" data-type=\"title-placeholder\" data-max=\"44\" fill=\"transparent\">.</tspan>\n    </text>\n\n    <text font-family=\"微软雅黑\" font-size=\"20\" font-weight=\"500\" fill=\"#000000\" style=\"dominant-baseline:middle;text-anchor:middle;\">\n      <tspan x=\"140\" y=\"163\" data-type=\"subTitle\" data-max=\"26\" data-len=\"8\"></tspan>\n      <tspan x=\"140\" y=\"163\" data-type=\"subTitle-placeholder\" data-max=\"26\" data-len=\"8\" fill=\"transparent\">.</tspan>\n    </text>\n\n    <g transform=\"translate(283, 118)\" font-family=\"微软雅黑\" font-size=\"20\" font-weight=\"normal\" fill=\"#FFFFFF\">\n      <text>\n        <tspan data-type=\"field\" data-len=\"14\" data-max=\"46\" data-count=\"1\" x=\"0\" y=\"81\"></tspan>\n        <tspan data-type=\"field-placeholder1\" data-max=\"46\" x=\"0\" y=\"81\" fill=\"transparent\">.</tspan>\n      </text>\n\n      <text>\n        <tspan data-type=\"field\" data-len=\"14\" data-max=\"46\" data-count=\"2\" x=\"0\" y=\"51\"></tspan>\n        <tspan data-type=\"field-placeholder2\" data-max=\"46\" x=\"0\" y=\"51\" fill=\"transparent\">.</tspan>\n      </text>\n      <text>\n        <tspan data-type=\"field\" data-len=\"14\" data-max=\"46\" data-count=\"2\" x=\"0\" y=\"98\"></tspan>\n        <tspan data-type=\"field-placeholder2\" data-max=\"46\" x=\"0\" y=\"98\" fill=\"transparent\">.</tspan>\n      </text>\n\n      <text>\n        <tspan data-type=\"field\" data-len=\"14\" data-max=\"46\" data-count=\"3\" x=\"0\" y=\"40\"></tspan>\n        <tspan data-type=\"field-placeholder3\" data-max=\"46\" x=\"0\" y=\"40\" fill=\"transparent\">.</tspan>\n      </text>\n      <text>\n        <tspan data-type=\"field\" data-len=\"14\" data-max=\"46\" data-count=\"3\" x=\"0\" y=\"90\"></tspan>\n        <tspan data-type=\"field-placeholder3\" data-max=\"46\" x=\"0\" y=\"90\" fill=\"transparent\">.</tspan>\n      </text>\n      <text>\n        <tspan data-type=\"field\" data-len=\"14\" data-max=\"46\" data-count=\"3\" x=\"0\" y=\"140\"></tspan>\n        <tspan data-type=\"field-placeholder3\" data-max=\"46\" x=\"0\" y=\"140\" fill=\"transparent\">.</tspan>\n      </text>\n\n      <text>\n        <tspan data-type=\"field\" data-len=\"14\" data-max=\"46\" data-count=\"4\" x=\"0\" y=\"21\"></tspan>\n        <tspan data-type=\"field-placeholder4\" data-max=\"46\" x=\"0\" y=\"21\" fill=\"transparent\">.</tspan>\n      </text>\n      <text>\n        <tspan data-type=\"field\" data-len=\"14\" data-max=\"46\" data-count=\"4\" x=\"0\" y=\"68\"></tspan>\n        <tspan data-type=\"field-placeholder4\" data-max=\"46\" x=\"0\" y=\"68\" fill=\"transparent\">.</tspan>\n      </text>\n      <text>\n        <tspan data-type=\"field\" data-len=\"14\" data-max=\"46\" data-count=\"4\" x=\"0\" y=\"114\"></tspan>\n        <tspan data-type=\"field-placeholder4\" data-max=\"46\" x=\"0\" y=\"114\" fill=\"transparent\">.</tspan>\n      </text>\n      <text>\n        <tspan data-type=\"field\" data-len=\"14\" data-max=\"46\" data-count=\"4\" x=\"0\" y=\"161\"></tspan>\n        <tspan data-type=\"field-placeholder4\" data-max=\"46\" x=\"0\" y=\"161\" fill=\"transparent\">.</tspan>\n      </text>\n    </g>\n  </g>\n</svg>",
-            "svgDemo": "https://dl-platform.effio.cn/0/qrcode/color5.svg",
-            "isColor": true,
-            "isLandscape": true,
-            "defaultColor": "#254569",
-            "defaultWidth": 120,
-            "defaultHeight": 80,
+            "id": "4XxixG511AP",
+            "type": 2,
+            "name": "标签2",
+            "svg": "<svg width=\"100%\" height=\"100%\" version=\"1.1\" viewBox=\"0 0 450 300\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\"><rect x=\"0\" y=\"0\" data-type=\"bg\" fill=\"#FFFFFF\" width=\"100%\" height=\"100%\"></rect><g><rect data-type=\"rect\" x=\"0\" y=\"0\" width=\"452\" height=\"178\" fill=\"#2B2A6F\" rx=\"0\" id=\"NrvFNMSHQIXrZQwRZyzFp\" transform=\"translate(-1,123)\"></rect><g x=\"0\" y=\"0\" id=\"pYlJvtikXjmDcjxKAFJMF\" transform=\"translate(18,25)\"><image href=\"\" data-type=\"logo\" width=\"73\" height=\"74\"></image></g><g id=\"bKtpZTaiHqLXOwKYFyiAL\" transform=\"translate(105,25)\"><text x=\"0\" y=\"0\" fill=\"#000000\" font-weight=\"bold\" font-style=\"normal\" font-family=\"-apple-system, &quot;Noto Sans&quot;, &quot;Helvetica Neue&quot;, Helvetica, &quot;Nimbus Sans L&quot;, Arial, &quot;Liberation Sans&quot;, &quot;PingFang SC&quot;, &quot;Hiragino Sans GB&quot;, &quot;Noto Sans CJK SC&quot;, &quot;Source Han Sans SC&quot;, &quot;Source Han Sans CN&quot;, &quot;Microsoft YaHei&quot;, &quot;Wenquanyi Micro Hei&quot;, &quot;WenQuanYi Zen Hei&quot;, &quot;ST Heiti&quot;, SimHei, &quot;WenQuanYi Zen Hei Sharp&quot;, sans-serif\" font-size=\"30\" dominant-baseline=\"\" text-anchor=\"\" transform=\"translate(7,0)\"><tspan data-type=\"title\" dy=\"30\" x=\"0\" data-max=\"37\" data-len=\"6\" y=\"0\"></tspan><tspan data-type=\"title-placeholder\" data-max=\"37\" fill=\"transparent\" x=\"0\" y=\"0\" dy=\"30\">十</tspan></text></g><g id=\"oNsvnrWngMOuaMUDUFxKA\" transform=\"translate(103,76)\"><text x=\"0\" y=\"0\" fill=\"#67717C\" font-weight=\"normal\" font-style=\"normal\" font-family=\"-apple-system, &quot;Noto Sans&quot;, &quot;Helvetica Neue&quot;, Helvetica, &quot;Nimbus Sans L&quot;, Arial, &quot;Liberation Sans&quot;, &quot;PingFang SC&quot;, &quot;Hiragino Sans GB&quot;, &quot;Noto Sans CJK SC&quot;, &quot;Source Han Sans SC&quot;, &quot;Source Han Sans CN&quot;, &quot;Microsoft YaHei&quot;, &quot;Wenquanyi Micro Hei&quot;, &quot;WenQuanYi Zen Hei&quot;, &quot;ST Heiti&quot;, SimHei, &quot;WenQuanYi Zen Hei Sharp&quot;, sans-serif\" font-size=\"20\" dominant-baseline=\"\" text-anchor=\"\" transform=\"translate(7,0)\"><tspan data-type=\"subTitle\" dy=\"20\" x=\"0\" data-max=\"54\" data-len=\"12\" y=\"0\"></tspan><tspan data-type=\"subTitle-placeholder\" data-max=\"54\" fill=\"transparent\" x=\"0\" y=\"0\" dy=\"20\">十</tspan></text></g><g id=\"FbMUWqKWKmWLEhxBGQOCQ\" transform=\"translate(17,139)\"><text x=\"0\" y=\"0\" fill=\"#FFFFFF\" font-weight=\"normal\" font-style=\"normal\" font-family=\"-apple-system, &quot;Noto Sans&quot;, &quot;Helvetica Neue&quot;, Helvetica, &quot;Nimbus Sans L&quot;, Arial, &quot;Liberation Sans&quot;, &quot;PingFang SC&quot;, &quot;Hiragino Sans GB&quot;, &quot;Noto Sans CJK SC&quot;, &quot;Source Han Sans SC&quot;, &quot;Source Han Sans CN&quot;, &quot;Microsoft YaHei&quot;, &quot;Wenquanyi Micro Hei&quot;, &quot;WenQuanYi Zen Hei&quot;, &quot;ST Heiti&quot;, SimHei, &quot;WenQuanYi Zen Hei Sharp&quot;, sans-serif\" font-size=\"22\" dominant-baseline=\"\" text-anchor=\"\" transform=\"translate(7,0)\"><tspan data-type=\"field\" dy=\"22\" x=\"0\" data-count=\"4\" data-max=\"53\" data-len=\"11\" y=\"0\"></tspan><tspan data-type=\"field-placeholder4\" data-max=\"53\" fill=\"transparent\" x=\"0\" y=\"0\" dy=\"22\">十</tspan></text></g><g id=\"dBBqgYhTfTJfdmPRncOQy\" transform=\"translate(16,180)\"><text x=\"0\" y=\"0\" fill=\"#FFFFFF\" font-weight=\"normal\" font-style=\"normal\" font-family=\"-apple-system, &quot;Noto Sans&quot;, &quot;Helvetica Neue&quot;, Helvetica, &quot;Nimbus Sans L&quot;, Arial, &quot;Liberation Sans&quot;, &quot;PingFang SC&quot;, &quot;Hiragino Sans GB&quot;, &quot;Noto Sans CJK SC&quot;, &quot;Source Han Sans SC&quot;, &quot;Source Han Sans CN&quot;, &quot;Microsoft YaHei&quot;, &quot;Wenquanyi Micro Hei&quot;, &quot;WenQuanYi Zen Hei&quot;, &quot;ST Heiti&quot;, SimHei, &quot;WenQuanYi Zen Hei Sharp&quot;, sans-serif\" font-size=\"22\" dominant-baseline=\"\" text-anchor=\"\" transform=\"translate(7,0)\"><tspan data-type=\"field\" dy=\"22\" x=\"0\" data-count=\"4\" data-max=\"53\" data-len=\"11\" y=\"0\"></tspan><tspan data-type=\"field-placeholder4\" data-max=\"53\" fill=\"transparent\" x=\"0\" y=\"0\" dy=\"22\">十</tspan></text></g><g id=\"FeRKXNEKxvGYtseDMzupj\" transform=\"translate(17,220)\"><text x=\"0\" y=\"0\" fill=\"#FFFFFF\" font-weight=\"normal\" font-style=\"normal\" font-family=\"-apple-system, &quot;Noto Sans&quot;, &quot;Helvetica Neue&quot;, Helvetica, &quot;Nimbus Sans L&quot;, Arial, &quot;Liberation Sans&quot;, &quot;PingFang SC&quot;, &quot;Hiragino Sans GB&quot;, &quot;Noto Sans CJK SC&quot;, &quot;Source Han Sans SC&quot;, &quot;Source Han Sans CN&quot;, &quot;Microsoft YaHei&quot;, &quot;Wenquanyi Micro Hei&quot;, &quot;WenQuanYi Zen Hei&quot;, &quot;ST Heiti&quot;, SimHei, &quot;WenQuanYi Zen Hei Sharp&quot;, sans-serif\" font-size=\"22\" dominant-baseline=\"\" text-anchor=\"\" transform=\"translate(7,0)\"><tspan data-type=\"field\" dy=\"22\" x=\"0\" data-count=\"4\" data-max=\"53\" data-len=\"11\" y=\"0\"></tspan><tspan data-type=\"field-placeholder4\" data-max=\"53\" fill=\"transparent\" x=\"0\" y=\"0\" dy=\"22\">十</tspan></text></g><g id=\"VtozJtFoYMzEhlzZyanMG\" transform=\"translate(17,260)\"><text x=\"0\" y=\"0\" fill=\"#FFFFFF\" font-weight=\"normal\" font-style=\"normal\" font-family=\"-apple-system, &quot;Noto Sans&quot;, &quot;Helvetica Neue&quot;, Helvetica, &quot;Nimbus Sans L&quot;, Arial, &quot;Liberation Sans&quot;, &quot;PingFang SC&quot;, &quot;Hiragino Sans GB&quot;, &quot;Noto Sans CJK SC&quot;, &quot;Source Han Sans SC&quot;, &quot;Source Han Sans CN&quot;, &quot;Microsoft YaHei&quot;, &quot;Wenquanyi Micro Hei&quot;, &quot;WenQuanYi Zen Hei&quot;, &quot;ST Heiti&quot;, SimHei, &quot;WenQuanYi Zen Hei Sharp&quot;, sans-serif\" font-size=\"22\" dominant-baseline=\"\" text-anchor=\"\" transform=\"translate(7,0)\"><tspan data-type=\"field\" dy=\"22\" x=\"0\" data-count=\"4\" data-max=\"53\" data-len=\"11\" y=\"0\"></tspan><tspan data-type=\"field-placeholder4\" data-max=\"53\" fill=\"transparent\" x=\"0\" y=\"0\" dy=\"22\">十</tspan></text></g><rect data-type=\"rect\" x=\"0\" y=\"0\" width=\"139\" height=\"139\" fill=\"#FFFFFF\" rx=\"6.95\" id=\"AjLWOHQWBaqwLiicqwRRc\" transform=\"translate(289,145)\"></rect><image x=\"0\" y=\"0\" data-type=\"qr\" href=\"\" width=\"129\" height=\"129\" id=\"yCOtjgjeFktovarkPtdhn\" transform=\"translate(294,150)\"></image></g></svg>",
+            "svgDemo": "https://dl-edisk.effio.cn/ptest/1522378481458163712.svg",
+            "isColor": false,
+            "isLandscape": false,
+            "defaultColor": "#FFFFFF",
+            "defaultWidth": 150,
+            "defaultHeight": 100,
             "hasLogo": true,
             "hasTitle": true,
             "hasSubTitle": true,
             "tagCount": 4
           },
-          "width": 120,
-          "height": 80,
-          "backgroudColor": "#254569",
-          "logo": "https://dl-edisk.effio.cn/test/1522036861732700160.png",
-          "title": "234234234",
-          "subTitle": "234234234234234",
+          "width": 150,
+          "height": 100,
+          "backgroudColor": "#FFFFFF",
+          "logo": "https://pinia.vuejs.org/logo.svg",
+          "title": "朱鸿",
+          "subTitle": "北包包包包包包包包包包包包包包包包包包包包包包包包",
           "tagFields": [
-            "dsfsdfsdfsdfsdfsdfsdfsdfsdf撒打算的",
-            "撒打算打算打算大撒打算打算的",
-            "撒打算打算打算大撒打算打算的",
-            "撒打算打算打算大撒打算打算的"
+            "🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸",
+            "🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸",
+            "🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥲",
+            "🥸🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸🥶🫠🥲🥸"
           ]
         }
-      },
+      }
     }
   },
 }
 </script>
 
 <template>
-  <QrCodeContainer :qrCodeInfo="qrCodeInfo" />
+  <div class="w-450px h-300px">
+    <QrCodeContainer :qrCodeInfo="qrCodeInfo" />
+  </div>
 </template>
