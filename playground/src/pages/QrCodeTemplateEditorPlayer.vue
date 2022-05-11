b63ea6f23a7d20ee20e1f203aba8fd405013afed
zhuh <zhuh@wingoht.com>
2022-05-11 16:11:14 +0800

    primarycolor

--- playground/src/pages/QrCodeTemplateEditorPlayer.vue
+++ playground/src/pages/QrCodeTemplateEditorPlayer.vue
@@ -27,7 +27,6 @@ export default {
 
 <template>
   <QrCodeTemplateEditor ref="templateEditor"
-    primaryColor="#fb7185"
     :fonts="[{ name: 'Fira Code', value: 'Fira Code' }, { name: 'JetBrains Mono', value: 'JetBrains Mono' }]"
     :colors="['#eea2a4', '#ee3f4d', '#12aa9c', '#57c3c2', '#ef6f48']"
   />
