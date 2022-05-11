0a0f3481fac3279c9d7e3ad75cde13677e936658
zhu-hong <zhxhyy@qq.com>
2022-05-11 21:59:55 +0800

    remove elementui

--- playground/main.js
+++ playground/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import App from './src/App.vue'
 import router from './src/router'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 
 // import YiYunQrCode from '../dist/lib.js'
 // import '../dist/style.css'
@@ -14,7 +12,6 @@ import 'virtual:windi.css'
 Vue.config.productionTip = false
 
 Vue.use(YiYunQrCode)
-Vue.use(ElementUI)
 
 new Vue({
   router,
