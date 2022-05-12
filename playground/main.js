4b9a76459717c8e512613cb8f0031ca74739a816
zhuh <zhuh@wingoht.com>
2022-05-12 13:03:45 +0800

    开发模式

--- playground/main.js
+++ playground/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './src/App.vue'
 import router from './src/router'
 
-import YiYunQrCode from '../dist/index.min.js'
-import '../dist/style.css'
+// import YiYunQrCode from '../dist/index.min.js'
+// import '../dist/style.css'
+
+import YiYunQrCode from '../src'
 
 import 'virtual:windi.css'
 
